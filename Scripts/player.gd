3ea74cb324b5c3056a073fd3931106074a7b0372
Jason Abbott <woebane@gmail.com>
2025-01-15 17:05:48 -0800

    Replaced player with animated dwarf

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -90,27 +90,32 @@ func _physics_process(delta: float) -> void:
 	if %State_Play.is_gameplay_active == false:
 		return
 	
-	look_at(get_global_mouse_position())
+	#look_at(get_global_mouse_position())
 	
 	fire_cooldown -= delta
 	if Input.is_action_pressed("fire_guns"):
 		if fire_cooldown <= 0:
 			fire_bullet()
 
-	if fire_cooldown <= 0:
-		gun_sprite.scale = Vector2.ONE
-	else:
-		var barrel_fraction : float = (fire_cooldown_max - max(fire_cooldown, 0)) / fire_cooldown_max
-		barrel_fraction *= barrel_fraction
-		gun_sprite.scale = Vector2(barrel_fraction, 1)
+	#if fire_cooldown <= 0:
+		#gun_sprite.scale = Vector2.ONE
+	#else:
+		#var barrel_fraction : float = (fire_cooldown_max - max(fire_cooldown, 0)) / fire_cooldown_max
+		#barrel_fraction *= barrel_fraction
+		#gun_sprite.scale = Vector2(barrel_fraction, 1)
 	
+	var anim : AnimatedSprite2D = (find_child("AnimatedSprite2D") as AnimatedSprite2D)
 	var acc_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if acc_dir == Vector2.ZERO:
 		var slow = (1 - delta)
 		slow = slow * slow
 		velocity = our_momentum * slow
+		if anim.is_playing():
+			anim.pause()
 	else:
 		velocity = (acc_dir * SPEED + our_momentum).normalized() * SPEED
+		if !anim.is_playing():
+			anim.play("default")
 
 	move_and_slide()
 	
