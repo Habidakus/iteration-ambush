bdfed1e3c63f86d4fcb4e3569a5667c7e63d768b
Jason Abbott <woebane@gmail.com>
2025-01-16 15:51:19 -0800

    Pre-cache fire pit shader, and made it a little more flamey

--- Scripts/StateMachine/StateMachineState_MainMenu.gd
+++ Scripts/StateMachine/StateMachineState_MainMenu.gd
@@ -1,5 +1,16 @@
 extends StateMachineState
 
+func enter_state() -> void:
+	super.enter_state()
+	do_shader_preload()
+	
+func do_shader_preload():
+	var shader : ShaderMaterial = load("res://Scene/fire_pit_material.tres")
+	var ca : CanvasItem = ColorRect.new()
+	ca.material = shader
+	add_child(ca)
+	ca.call_deferred("queue_free")
+	
 func _on_button_button_up() -> void:
 	%State_Play.build_seed = 1
 	our_state_machine.switch_state("State_Play")
